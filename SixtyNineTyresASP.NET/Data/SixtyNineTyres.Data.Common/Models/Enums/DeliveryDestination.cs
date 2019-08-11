265795c039c1101dea77cc6a76c4e2b6f17eb006
danailstratirv <stratyd@abv.bg>
2019-08-11 23:30:02 +0300

    New classes added

---
+++ SixtyNineTyresASP.NET/Data/SixtyNineTyres.Data.Common/Models/Enums/DeliveryDestination.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SixtyNineTyres.Data.Common.Models.Enums
+{
+   public enum DeliveryDestination
+    {
+        Home = 1,
+        CourierOffice = 2
+    }
+}
---
+++ SixtyNineTyresASP.NET/Data/SixtyNineTyres.Data.Common/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SixtyNineTyres.Data.Common.Models
+{
+   public class Order
+    {
+        public string Id { get; set; }
+
+        public DateTime DateOfCreation { get; set; }
+
+        public DateTime DeliveryDate { get; set; }
+
+        public DeliveryDestination DeliveryDestination { get; set; }
+    }
+}
